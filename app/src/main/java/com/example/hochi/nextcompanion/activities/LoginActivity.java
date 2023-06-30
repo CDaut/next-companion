a6240dd736fbe97a94f06dded72c498d97027f6f
CDaut <git@cdaut.de>
2023-07-01 00:48:49 +0200

    fixed gradle linter Errors

--- app/src/main/java/com/example/hochi/nextcompanion/activities/LoginActivity.java
+++ app/src/main/java/com/example/hochi/nextcompanion/activities/LoginActivity.java
@@ -4,7 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.TextUtils;
